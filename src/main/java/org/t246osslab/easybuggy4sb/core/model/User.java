fc06d20d4e0501db898f447e7313b11bc7c1cdec
Diogo-fj-rocha <diogo.fj.rocha@gmail.com>
2024-11-08 18:08:59 +0000

    Removed password

--- src/main/java/org/t246osslab/easybuggy4sb/core/model/User.java
+++ src/main/java/org/t246osslab/easybuggy4sb/core/model/User.java
@@ -19,7 +19,6 @@ public class User implements Serializable {
     private String userId = null;
     private String name = null;
     @Attribute(name = "userPassword")
-    private String password = null;
     private String secret = null;
     private String phone = null;
     private String mail = null;
